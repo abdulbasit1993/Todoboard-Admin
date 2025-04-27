0314024a9d3f4dcf92f50e29022fbebc5258b37a
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2025-04-28 01:31:39 +0500

    Completed the Header section UI

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Image from "next/image";
+import us from "@/assets/images/us-flag.png";
+import sampleUserImg from "@/assets/images/sample-user.jpg";
+import { Bell } from "lucide-react";
 
 const Header = () => {
   return (
@@ -7,6 +11,34 @@ const Header = () => {
         <h1 className="text-lg sm:text-xl lg:text-2xl font-semibold text-gray-100">
           Dashboard
         </h1>
+
+        <div className="flex items-center space-x-3 sm:space-x-6">
+          <Image
+            src={us}
+            alt="country flag"
+            width={25}
+            height={18}
+            className="rounded-full shadow-md cursor-pointer"
+          />
+
+          <div className="relative">
+            <Bell className="w-5 sm:w-6 h-5 sm:h-6 text-gray-300 cursor-pointer hover:text-white" />
+          </div>
+
+          <div className="flex items-center space-x-2 sm:space-x-3">
+            <Image
+              src={sampleUserImg}
+              alt={"admin"}
+              width={35}
+              height={35}
+              className="rounded-full border border-gray-600"
+            />
+
+            <span className="hidden sm:block text-gray-100 font-medium">
+              John Mark
+            </span>
+          </div>
+        </div>
       </div>
     </header>
   );
