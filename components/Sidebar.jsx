23ecb3441f2beb057665519a6c7046b53adbb411
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2025-04-28 23:35:38 +0500

    completed the sidebar design with toggle button, starting work on HomePage

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,6 +7,7 @@ import {
   House,
   Info,
   Mail,
+  Menu,
   Settings,
   ShoppingBag,
   ShoppingCart,
@@ -29,12 +30,24 @@ const ICONS = {
 };
 
 const Sidebar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const [sidebarItems, setSidebarItems] = useState(appData?.sidebarItems);
   const pathname = usePathname();
 
   return (
-    <div className="relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 w-64">
-      <div className="h-full bg-[#1e1e1e] backdrop-blur-md p- flex flex-col border-r border-[#2f2f2f]">
+    <div
+      className={`relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 ${
+        isSidebarOpen ? "w-64" : "w-20"
+      }`}
+    >
+      <div className="h-full bg-[#1e1e1e] backdrop-blur-md p-4 flex flex-col border-r border-[#2f2f2f]">
+        <button
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className="p-2 rounded-full hover:bg-[#2f2f2f] transition-colors max-w-fit cursor-pointer"
+        >
+          <Menu size={24} />
+        </button>
+
         <nav className="mt-8 flex-grow">
           {sidebarItems?.map((item) => {
             const IconComponent = ICONS[item.icon];
@@ -47,7 +60,10 @@ const Sidebar = () => {
                   }`}
                 >
                   <IconComponent size={20} style={{ minWidth: "20px" }} />
-                  <span className="ml-4 whitespace-nowrap">{item.name}</span>
+
+                  {isSidebarOpen && (
+                    <span className="ml-4 whitespace-nowrap">{item.name}</span>
+                  )}
                 </div>
               </Link>
             );
