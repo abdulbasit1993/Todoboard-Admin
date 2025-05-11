e5ba837f1a627320bcb41076a246cd53530acd63
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2025-05-12 00:11:52 +0500

    added Base URL of backend for production deployment

--- constants/apiUrl.js
+++ constants/apiUrl.js
@@ -1,1 +1,6 @@
-export const BASE_URL = "http://localhost:3001/api";
+const base_urls_list = {
+  dev: "http://localhost:3001/api",
+  production: "https://todoboard-backend.onrender.com/api",
+};
+
+export const BASE_URL = base_urls_list?.production;
