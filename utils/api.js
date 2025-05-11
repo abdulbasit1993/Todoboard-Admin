33038b2e296ce03f32fb77bfa8e7fd13ddedb8db
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2025-05-12 01:35:29 +0500

    increased timeout in axios interceptor

--- utils/api.js
+++ utils/api.js
@@ -3,7 +3,7 @@ import { BASE_URL } from "@/constants/apiUrl";
 
 const api = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
+  timeout: 90000,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
