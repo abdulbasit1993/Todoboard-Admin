c001cb6635b1f4b8bb5cb2c8d1b7d68d6c4a23e2
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2025-05-18 00:56:58 +0500

    fixed redirection for non-authenticated users

--- components/layouts/withProtectedLayout.jsx
+++ components/layouts/withProtectedLayout.jsx
@@ -1,12 +1,39 @@
-import React from "react";
-import ProtectedLayout from "./ProtectedLayout";
+"use client";
 
-export default function withProtectedLayout(PageComponent) {
-  return function WrappedComponent(props) {
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Header from "../Header";
+import Sidebar from "../Sidebar";
+
+export default function withProtectedLayout(Component) {
+  return function ProtectedComponent(props) {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        router.replace("/login");
+      } else {
+        setIsLoading(false);
+      }
+    }, []);
+
+    if (isLoading) return null;
+
     return (
-      <ProtectedLayout>
-        <PageComponent {...props} />
-      </ProtectedLayout>
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar />
+        <div className="flex flex-col flex-1 overflow-auto">
+          <div className="max-w-7xl mx-auto w-full">
+            <Header />
+            <main>
+              <Component {...props} />
+            </main>
+          </div>
+        </div>
+      </div>
     );
   };
 }
